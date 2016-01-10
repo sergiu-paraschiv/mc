21c8e0cb49c09486fe74a1e7dec99419c5fee080
unknown <sergiuspk@gmail.com>
2016-01-10 15:33:24 +0200

    f1

--- public/bundle.js
+++ public/bundle.js
@@ -15,6 +15,8 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
+consol.log('x', _websocket2.default);
+
 var CECService = function () {
     function CECService() {
         _classCallCheck(this, CECService);
